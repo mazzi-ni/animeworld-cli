1a3fe058d794ea272b799181c21a5a8d3172889b
andrea mazzini <andrea.mazzini@veycore.com>
2025-01-20 17:34:35 +0100

    fix: insert cookie in requests

--- animeworld.py
+++ animeworld.py
@@ -1,158 +1,187 @@
+import re
 from bs4 import BeautifulSoup
-from os import name, path, makedirs
-from yaspin import yaspin
-import libs.colorify as cc
+from os import path, makedirs
 import requests
-import sys
 
 class Anime:
-	BASE_URL = 'https://www.animeworld.so'
-	BASE_API_URL = 'https://www.animeworld.so/api/episode/serverPlayerAnimeWorld?id='
+    BASE_URL = "https://www.animeworld.so"
+    BASE_API_URL = "https://www.animeworld.so/api/episode/serverPlayerAnimeWorld?id="
 
-	def __init__(self):
-		self.name = '';
-		self.anime_data = {}
-	
-	def search(self, name):
-		url = self.BASE_URL + '/search?keyword=' + name.replace(' ', '+');
-		
-		if name == 'ongoing':
-			url = self.BASE_URL + '/' + name;
+    def __init__(self):
+        self.name = ""
+        self.anime_data = {}
+        self.session = requests.session()
 
-		page = requests.get(url);
-		soup = BeautifulSoup(page.content, 'html.parser');
-		
-		anime_list = []
-		for link in soup.find_all('a'):
-			if 'name' in str(link.get('class')):
-				anime_list.append(link.get('href'));
+    def get_cookie(self, url):
+        page = self.session.get(url)
 
-		# last 6*3 + 6 remain because are that anime in the bottom
-		anime_list = anime_list[:-24]
-		self.name = name;
-		# DEBUG:
-		# print(anime_list);
-		# print(len(anime_list));
+        cookie_match = re.search(r'SecurityAW-gl=[a-f0-9]{32}', str(page.content));
+        if cookie_match:
+            self.session.cookies.set("SecurityAW-gl", cookie_match.group().split('=')[1])
+            return cookie_match.group();
 
-		return anime_list;
+        return "";
 
-	def anime_page(self, link):
-		url = link
-		if not(self.BASE_URL in link):
-			url = self.BASE_URL + link
+    def get_soup(self, url):
+        cookie = self.get_cookie(url);
+        
+        page = self.session.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        return soup;
 
-		page = requests.get(url);
-		soup = BeautifulSoup(page.content, 'html.parser');
-		
-		anime_metadata = {}
-		for col in soup.find_all('dl'):
-			for tag, value in zip(col.find_all('dt'), col.find_all('dd')):
-				t =  str(tag.string).replace('\r\n', '').replace(' ', '').replace(':','');
-				v =  str(value.string).replace('\r\n', '').replace(' ', '');
-				
-				anime_metadata.update({t:v});
-		
-		# DEBUG:
-		# print(bool(anime_metadata))
-		
-		# TODO: sistemare le ridondanze in questo codice ↓
-		
-		eps = []
-		for div in soup.find_all('div'):
-			if 'server active' in str(div):
-				eps = div.find_all('a');
-		
-		episode_id = []
-		for e in eps:
-			episode_id.append(e.get('data-id'));
+    def search(self, name):
+        url = self.BASE_URL + "/search?keyword=" + name.replace(" ", "+")
 
-		# DEBUG:
-		# print(episode_id)
-		# print(len(episode_id))
-		
-		if not(bool(anime_metadata)):
-			raise ValueError('url not correct')
-		
-		# convert anime duration in int
-		if 'he' in anime_metadata['Durata'] and 'min' in anime_metadata['Durata']:
-			h_and_min = anime_metadata['Durata'].split('he', 1);
-			try:
-				min = int(h_and_min[1].replace('min',''));
-				h = int(h_and_min[0]);
-				anime_metadata['Durata'] = h * 60 + min;
-			except:
-				anime_metadata['Durata'] = 0;
+        if name == "ongoing":
+            url = self.BASE_URL + "/" + name
+        
+        soup = self.get_soup(url);
+        if soup == None:
+            return;
 
-		elif 'min/ep' in anime_metadata['Durata']:
-			anime_metadata['Durata'] = anime_metadata['Durata'].replace('min/ep', '');
-			try:
-				anime_metadata['Durata'] = int(anime_metadata['Durata'])
-			except:
-				anime_metadata['Durata'] = 0;
+        anime_list = []
+        for link in soup.find_all("a"):
+            if "name" in str(link.get("class")):
+                anime_list.append(link.get("href"))
 
-		else:
-			anime_metadata['Durata'] = 0;
+        # last 6*3 + 6 remain because are that anime in the bottom
+        anime_list = anime_list[:-24]
+        self.name = name
+        # DEBUG:
+        # print(anime_list);
+        # print(len(anime_list));
 
-		self.anime_data = {
-				"eps_id" : episode_id,
-				"eps_n" : len(episode_id), #anime_metadata['Episodi'],
-				"stagione" : anime_metadata['Stagione'],
-				"data_uscita" : anime_metadata['DatadiUscita'],
-				"eps_durata" : anime_metadata['Durata'],
-				"Audio" : anime_metadata['Audio']
-			}
+        return anime_list
 
-		# DEBUG:
-		# print(self.anime_data);
+    def anime_page(self, link):
+        url = link
+        if not (self.BASE_URL in link):
+            url = self.BASE_URL + link
+        
+        soup = self.get_soup(url);
+        if soup == None:
+            return;
 
-		return self.anime_data;
+        anime_metadata = {}
+        for col in soup.find_all("dl"):
+            for tag, value in zip(col.find_all("dt"), col.find_all("dd")):
+                t = (
+                    str(tag.string)
+                    .replace("\r\n", "")
+                    .replace(" ", "")
+                    .replace(":", "")
+                )
+                v = str(value.string).replace("\r\n", "").replace(" ", "")
 
-	def video_page(self, id):
-		page = requests.get(self.BASE_API_URL + id);
-		soup = BeautifulSoup(page.content, 'html.parser');
+                anime_metadata.update({t: v})
 
-		source = soup.find('source').get('src');
-		
-		# DEBUG:
-		# print(source);
+        # DEBUG:
+        # print(bool(anime_metadata))
 
-		if source == None:
-			print('=> Err id non valido');
-			return 'nope';
-		
-		return source;
+        # TODO: sistemare le ridondanze in questo codice ↓
 
-	def dowload_anime(self, link):
-		home = path.expanduser('~/Videos');
-		source_url = link.split('/');
-		file_name = path.join(home, source_url[-2], source_url[-1]);
-		
-		if not path.exists(path.dirname(file_name)):
-			try:
-				makedirs(path.dirname(file_name))
-			except OSError as exc: # Guard against race condition
-				if exc.errno != errno.EEXIST:
-					raise
+        eps = []
+        for div in soup.find_all("div"):
+            if "server active" in str(div):
+                eps = div.find_all("a")
 
-		# DEBUG:
-		# print(source_url)
-		# print(file_name)
-		
-		with open(file_name, "wb") as f:
-			response = requests.get(link, stream=True)
-			total_length = response.headers.get('content-length')
+        episode_id = []
+        for e in eps:
+            episode_id.append(e.get("data-id"))
 
-			# DEBUG:
-			# print(total_length);
+        # DEBUG:
+        # print(episode_id)
+        # print(len(episode_id))
 
-			if total_length is None:
-				f.write(response.content)
-			else:
-				dl = 0
-				total_length = int(total_length)
-					
-				for data in response.iter_content(chunk_size=4096):
-					dl += len(data)
-					f.write(data)
+        if not (bool(anime_metadata)):
+            raise ValueError("url not correct")
 
+        # convert anime duration in int
+        if "he" in anime_metadata["Durata"] and "min" in anime_metadata["Durata"]:
+            h_and_min = anime_metadata["Durata"].split("he", 1)
+            try:
+                min = int(h_and_min[1].replace("min", ""))
+                h = int(h_and_min[0])
+                anime_metadata["Durata"] = h * 60 + min
+            except:
+                anime_metadata["Durata"] = 0
+
+        elif "min/ep" in anime_metadata["Durata"]:
+            anime_metadata["Durata"] = anime_metadata["Durata"].replace("min/ep", "")
+            try:
+                anime_metadata["Durata"] = int(anime_metadata["Durata"])
+            except:
+                anime_metadata["Durata"] = 0
+
+        else:
+            anime_metadata["Durata"] = 0
+
+        self.anime_data = {
+            "eps_id": episode_id,
+            "eps_n": len(episode_id),  # anime_metadata['Episodi'],
+            "stagione": anime_metadata["Stagione"],
+            "data_uscita": anime_metadata["DatadiUscita"],
+            "eps_durata": anime_metadata["Durata"],
+            "Audio": anime_metadata["Audio"],
+        }
+
+        # DEBUG:
+        # print(self.anime_data);
+
+        return self.anime_data
+
+    def video_page(self, id):
+        url = self.BASE_API_URL + id
+
+        soup = self.get_soup(url);
+        source = soup.find("source").get("src")
+
+        if source == None:
+            print("=> Err id non valido")
+            return "nope"
+
+        return source
+
+    def original_video_url(self, url):
+        soup = self.get_soup(url);
+        video_url = soup.find('source').get('src');
+
+        if video_url == None:
+            raise ValueError("ERROR url non valido")
+
+        return video_url
+
+
+    def dowload_anime(self, link):
+        home = path.expanduser("~/Videos")
+        source_url = link.split("/")
+        file_name = path.join(home, source_url[-2], source_url[-1])
+
+        if not path.exists(path.dirname(file_name)):
+            try:
+                makedirs(path.dirname(file_name))
+            except OSError as exc:  # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+
+        # DEBUG:
+        # print(source_url)
+        # print(file_name)
+
+        with open(file_name, "wb") as f:
+            response = requests.get(link, stream=True)
+            total_length = response.headers.get("content-length")
+
+            # DEBUG:
+            # print(total_length);
+
+            if total_length is None:
+                f.write(response.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+
+                for data in response.iter_content(chunk_size=4096):
+                    dl += len(data)
+                    f.write(data)
 
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
 from typing_extensions import Annotated
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from alive_progress import alive_bar, alive_it
+from concurrent.futures import ThreadPoolExecutor
 from yaspin import yaspin
 import questionary as q
 import typer
-import mpv 
-import requests
+import mpv
 
 from animeworld import Anime
 import libs.color as c
@@ -17,253 +15,261 @@ import libs.colorify as cc
 app = typer.Typer()
 anime = Anime()
 
+
 @app.command()
 def search(anime_name: str):
-	"""
-	Search and play an Anime in Animeworld.tv
-	"""
+    """
+    Search and play an Anime in Animeworld.tv
+    """
 
-	anime_list = anime.search(anime_name)
-	anime_list_option = [];
+    anime_list = anime.search(anime_name)
+    anime_list_option = []
 
-	for anime_url in anime_list:
-		name = anime_url.replace('/play/', '').replace('-', ' ')[:-6]
-		anime_list_option.append(name)
+    for anime_url in anime_list:
+        name = anime_url.replace("/play/", "").replace("-", " ")[:-6]
+        anime_list_option.append(name)
 
-	select = q.select('Select an Anime: ', choices = anime_list_option).ask()
-	
-	# DEBUG:
-	# print(anime_list[anime_list_option.index(select)])
+    select = q.select("Select an Anime: ", choices=anime_list_option).ask()
 
-	return play(anime_list[anime_list_option.index(select)])
+    # DEBUG:
+    # print(anime_list[anime_list_option.index(select)])
+
+    return play(anime_list[anime_list_option.index(select)])
+
 
 @app.command()
 def play(url: Annotated[str, typer.Option()]):
-	"""
-	Play an Anime from Animeworld.tv url with mpv
-	"""
-	anime_data = anime.anime_page(url);
-	
-	def validate(ep):
-		if len(ep) > 0:
-			try:
-				ep = int(ep)
-				if ep <= anime_data['eps_n'] and ep > 0:
-					return True
-				else:
-					return "Please enter a correct value"
-			except Exception as err:
-				return "Please enter a correct value"
-		
-		return "Please enter a value"
-	
-	ep = q.text(
-			'Select an Episode to start: [1 ... ' + str(anime_data['eps_n']) + ']',
-			default = '1',
-			validate = validate,
-		).ask()
+    """
+    Play an Anime from Animeworld.tv url with mpv
+    """
+    anime_data = anime.anime_page(url)
 
-	play_id(int(ep) - 1);
-	
+    def validate(ep):
+        if len(ep) > 0:
+            try:
+                ep = int(ep)
+                if ep <= anime_data["eps_n"] and ep > 0:
+                    return True
+                else:
+                    return "Please enter a correct value"
+            except Exception as err:
+                return "Please enter a correct value"
+
+        return "Please enter a value"
+
+    ep = q.text(
+        "Select an Episode to start: [1 ... " + str(anime_data["eps_n"]) + "]",
+        default="1",
+        validate=validate,
+    ).ask()
+
+    play_id(int(ep) - 1)
+
 
 def play_id(ep_index):
-	while True:
-		print('\n' + c.BOLD + c.CGREEN2 + ' » episode: ' + str(ep_index + 1) + c.RESET)
-		print(' » url: ' + anime.BASE_API_URL + anime.anime_data['eps_id'][ep_index]);
-		
-		player = mpv.MPV(
-			input_default_bindings=True,
-			input_vo_keyboard=True,
-			osc=True,
-			ytdl=True
-		)
-		player['vo'] = 'gpu';
-		player.fullscreen = False;
-		
-		@player.property_observer('time-pos')
-		def time_observer(_name, value):
-			if value != None:
-				# alive_it(value)
-				# print('Now playing at ' + str(value) + 's', end='\r')
-				cc.progress_bar(value, anime.anime_data['eps_durata']);
+    while True:
+        print("\n" + c.BOLD + c.CGREEN2 + " » episode: " + str(ep_index + 1) + c.RESET)
+        print(" » url: " + anime.BASE_API_URL + anime.anime_data["eps_id"][ep_index])
 
-		try:
-			player.play(anime.BASE_API_URL + anime.anime_data['eps_id'][ep_index]);
-			player.wait_for_playback();
-		except Exception as err:
-			print('\n=> ERROR: esercuzione interrotta');
-			print(err);
+        player = mpv.MPV(
+            input_default_bindings=True, 
+            input_vo_keyboard=True, 
+            osc=True, 
+            ytdl=True
+        )
+        player["vo"] = "gpu"
+        player.fullscreen = False
+        
 
-		player.terminate();
-		del player;
+        @player.property_observer("time-pos")
+        def time_observer(_name, value):
+            if value != None:
+                # alive_it(value)
+                # print('Now playing at ' + str(value) + 's', end='\r')
+                cc.progress_bar(value, anime.anime_data["eps_durata"])
 
-		print('')
-		ep_index += 1;
-		if ep_index >= anime.anime_data['eps_n']:
-			print(c.BOLD + c.CGREEN2 + ' » fine serie' + c.RESET);
-			break;
-		
-		retry = q.confirm('Next Episode: ').ask()
+        try:
+            url = anime.original_video_url(anime.BASE_API_URL + anime.anime_data["eps_id"][ep_index])
+            player.play(url)
+            player.wait_for_playback()
+        except Exception as err:
+            print("\n=> ERROR: esercuzione interrotta")
+            print(err)
 
-		if not(retry):
-			break;
+        player.terminate()
+        del player
+
+        print("")
+        ep_index += 1
+        if ep_index >= anime.anime_data["eps_n"]:
+            print(c.BOLD + c.CGREEN2 + " » fine serie" + c.RESET)
+            break
+
+        retry = q.confirm("Next Episode: ").ask()
+
+        if not (retry):
+            break
+
 
 @app.command()
 def recent():
-	search('ongoing');
-
+    search("ongoing")
 
 
 @app.command()
 def ascii():
-	"""
-	Print Ascii font from font.txt
-	"""
-	cc.ascii_font();
+    """
+    Print Ascii font from font.txt
+    """
+    cc.ascii_font()
+
 
 @app.command()
 def download(
-		name: Annotated[str, typer.Option()] = None, 
-		multi_thread: Annotated[bool, typer.Option()] = False, 
-		ascii: Annotated[bool, typer.Option()] = False):
-	"""
-	Download an Anime from Animeworld.tv
-	"""
-	if ascii:
-		cc.ascii_font()
+    name: Annotated[str, typer.Option()] = None,
+    multi_thread: Annotated[bool, typer.Option()] = False,
+    ascii: Annotated[bool, typer.Option()] = False,
+):
+    """
+    Download an Anime from Animeworld.tv
+    """
+    if ascii:
+        cc.ascii_font()
 
-	anime_list = []
-	if name == None:
-		while len(anime_list) == 0:
-			name = q.text(
-				'Insert the name of the Anime',
-				validate = lambda text: True if len(text) > 0 else 'Error: insert something',
-			).ask()
-		
-			anime_list = anime.search(name);
-			if len(anime_list) == 0:
-				print('=> Error: Anime not found')
-	
-	else:
-		anime_list = anime.search(name);
-		if len(anime_list) == 0:
-			print('=> Error: Anime not found')
-			return 0;
-		
-	
-	anime_list_option = [];
-	for anime_url in anime_list:
-		name = anime_url.replace('/play/', '').replace('-', ' ')[:-6]
-		anime_list_option.append(name)
+    anime_list = []
+    if name == None:
+        while len(anime_list) == 0:
+            name = q.text(
+                "Insert the name of the Anime",
+                validate=lambda text: True
+                if len(text) > 0
+                else "Error: insert something",
+            ).ask()
 
-	select = q.select('Select an Anime: ', choices = anime_list_option).ask()
-	anime.anime_page(anime_list[anime_list_option.index(select)]);
-	
-	def validate(text):
-		if len(text) > 0:
-			if ' ' in text:
-				text = text.replace(' ', '');
-			
-			if '-' in text:
-				text = text.split('-')
-				try:
-					text[0] = int(text[0])
-					text[1] = int(text[1])
-					if text[0] >= 1 and text[1] <= anime.anime_data['eps_n']:
-						return True
-				except:
-					return "Please enter correct a value"
-		return "Please enter a value"
+            anime_list = anime.search(name)
+            if len(anime_list) == 0:
+                print("=> Error: Anime not found")
 
+    else:
+        anime_list = anime.search(name)
+        if len(anime_list) == 0:
+            print("=> Error: Anime not found")
+            return 0
 
-	ep_range = q.text(
-		'Select range of Episode to download: [1-' + str(anime.anime_data['eps_n']) + ']',
-		default = '1-' + str(anime.anime_data['eps_n']),
-		validate = validate,
-	).ask()
-	
-	ep_range = list(map(int, ep_range.split('-')))
-	ep_range[0] = ep_range[0] - 1
-	range_ep_id = anime.anime_data['eps_id'][ep_range[0]:ep_range[1]]
-	
-	# DEBUG:
-	# print(ep_id_dowload)
+    anime_list_option = []
+    for anime_url in anime_list:
+        name = anime_url.replace("/play/", "").replace("-", " ")[:-6]
+        anime_list_option.append(name)
 
-	if multi_thread:
-		multithread_download(range_ep_id)
-	else:	
-		singlethread_download(range_ep_id)
-	
-	print('\n=> finish')
+    select = q.select("Select an Anime: ", choices=anime_list_option).ask()
+    anime.anime_page(anime_list[anime_list_option.index(select)])
+
+    def validate(text):
+        if len(text) > 0:
+            if " " in text:
+                text = text.replace(" ", "")
+
+            if "-" in text:
+                text = text.split("-")
+                try:
+                    text[0] = int(text[0])
+                    text[1] = int(text[1])
+                    if text[0] >= 1 and text[1] <= anime.anime_data["eps_n"]:
+                        return True
+                except:
+                    return "Please enter correct a value"
+        return "Please enter a value"
+
+    ep_range = q.text(
+        "Select range of Episode to download: [1-"
+        + str(anime.anime_data["eps_n"])
+        + "]",
+        default="1-" + str(anime.anime_data["eps_n"]),
+        validate=validate,
+    ).ask()
+
+    ep_range = list(map(int, ep_range.split("-")))
+    ep_range[0] = ep_range[0] - 1
+    range_ep_id = anime.anime_data["eps_id"][ep_range[0] : ep_range[1]]
+
+    # DEBUG:
+    # print(ep_id_dowload)
+
+    if multi_thread:
+        multithread_download(range_ep_id)
+    else:
+        singlethread_download(range_ep_id)
+
+    print("\n=> finish")
 
 
 def singlethread_download(range_ep_id):
-	with yaspin(text="Loading", color="yellow") as sp:
-		for id in range_ep_id:
-			source = anime.video_page(id)
-			anime.dowload_anime(source)
-			sp.write('=> ep ' + source.split('/')[-1] +  ' download complete')
+    with yaspin(text="Loading", color="yellow") as sp:
+        for id in range_ep_id:
+            source = anime.video_page(id)
+            anime.dowload_anime(source)
+            sp.write("=> ep " + source.split("/")[-1] + " download complete")
 
-	
+
 def multithread_download(range_ep_id):
-	futures = []
-	
-	with yaspin(text="Downloading", color="yellow") as sp:
-		with ThreadPoolExecutor(max_workers=4) as executor:
-			try:
-				for id in range_ep_id:
-					source = anime.video_page(id)
-					future = executor.submit(anime.dowload_anime, source)
-					futures.append(future)
-				
-				count = len(futures)
-				while count != 0:
-					for f in futures:
-						if f.done():
-							count -= 1
-							sp.write('=> ep ' + str(len(futures) - count) + ' download complete')
+    futures = []
 
-			except KeyboardInterrupt:
-				executor._threads.clear()
-				raise typer.Exit()
+    with yaspin(text="Downloading", color="yellow") as sp:
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            try:
+                for id in range_ep_id:
+                    source = anime.video_page(id)
+                    future = executor.submit(anime.dowload_anime, source)
+                    futures.append(future)
 
-	
+                count = len(futures)
+                while count != 0:
+                    for f in futures:
+                        if f.done():
+                            count -= 1
+                            sp.write(
+                                "=> ep "
+                                + str(len(futures) - count)
+                                + " download complete"
+                            )
+
+            except KeyboardInterrupt:
+                executor._threads.clear()
+                raise typer.Exit()
+
+
 @app.callback(invoke_without_command=True)
 def main(ctx: typer.Context):
-	if ctx.invoked_subcommand != None:
-		return 0
+    if ctx.invoked_subcommand != None:
+        return 0
 
-	ascii();
-	
-	anime_list = []
-	name = ''
+    ascii()
 
-	while len(anime_list) == 0:
-		name = q.text(
-				'Insert the name of the Anime',
-				validate = lambda text: True if len(text) > 0 else 'Error: insert something',
-			).ask()
-		
-		anime_list = anime.search(name);
-		if len(anime_list) == 0:
-			print('=> Error: Anime not found')
-	
-	search(name)
+    anime_list = []
+    name = ""
 
-	
+    while len(anime_list) == 0:
+        name = q.text(
+            "Insert the name of the Anime",
+            validate=lambda text: True if len(text) > 0 else "Error: insert something",
+        ).ask()
 
-if __name__ == '__main__':
-	try:
-		app()
-	except KeyboardInterrupt:
-		print("=> Cancelled by user")
-	except ValueError as err:
-		print('=> Errore:', err);
-	except AttributeError:
-		pass
-	except Exception as err:
-		print('=> Errore:', type(err).__name__, err);
+        anime_list = anime.search(name)
+        if len(anime_list) == 0:
+            print("=> Error: Anime not found")
+
+    search(name)
 
 
-
+if __name__ == "__main__":
+    try:
+        app()
+    except KeyboardInterrupt:
+        print("=> Cancelled by user")
+    except ValueError as err:
+        print("=> Errore:", err)
+    except AttributeError:
+        pass
+    except Exception as err:
+        print("=> Errore:", type(err).__name__, err)
 
